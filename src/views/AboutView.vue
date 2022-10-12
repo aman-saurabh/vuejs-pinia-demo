15a11011cdfc8d237aff702b7792dfafc200e2bf
techpassel <techpasselorg@gmail.com>
2022-10-12 23:05:03 +0530

    used store in about page also

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
     <h1>About</h1>
+    <button class="counter-button">
+      {{count}}
+    </button>
   </div>
 </template>
+
+<script setup>
+import { storeToRefs } from 'pinia';
+import { useCounterStore } from '../stores/counter';
+
+const counterStore = useCounterStore();
+//For state and Getters(i.e. computed properties we need to use 'storeToRefs'  but not for actions)
+const { count } = storeToRefs(counterStore);
+</script>
+
+<style scoped>
+.counter-button {
+  font-size: 50px;
+}
+</style>
